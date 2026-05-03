107c030c3c743ddf02ecb7807a42885c30f07b73
Jeffrey Mello <mrjeffreymello@gmail.com>
2026-05-03 13:37:44 -0400

    switch to mdx and move some more articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 import vue from "@astrojs/vue";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [vue()],
-  build: {
-    inlineStylesheets: "always",
-  },
+  integrations: [vue(), mdx()],
 });
